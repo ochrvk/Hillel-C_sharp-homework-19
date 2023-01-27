b7e7fda98fffdd1ce3314aec617c70518ebc4b81
Oleksandr Cherevko <oc.oleksandr.cherevko@outlook.com>
2023-01-27 14:05:53 +0200

    first commit

---
+++ Homework_19/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Threading.Tasks;
+
+class Account
+{
+    public int Balance { get; set; }
+    public int Id { get; set; }
+}
+
+
+class Program
+{
+    static void Transfer(Account account1, Account account2)
+    {
+        List<Task> tasks = new List<Task>();
+        object locker = new object();
+
+        for (int j = 0; j < 2; j++)
+        {
+            tasks.Add(Task.Run(() =>
+            {
+                for (int i = 0; i < 1000000; i++)
+                {
+                    lock (locker)
+                    {
+                        account1.Balance--;
+                        account2.Balance++;
+                    }
+                }
+            }));
+        }
+        //Thanks for this ;)
+        Task.WaitAll(tasks.ToArray());
+    }
+
+    static void Main(string[] args)
+    {
+
+        Account account1 = new Account();
+        account1.Balance = 2000000;
+        account1.Id = 100;
+
+        Account account2 = new Account();
+        account2.Balance = 0;
+        account2.Id = 200;
+
+        Console.WriteLine("Before transfer: ");
+        Console.WriteLine($"счет {account1.Id}: {account1.Balance}");
+        Console.WriteLine($"счет {account2.Id}: {account2.Balance}");
+
+        Transfer(account1, account2);
+
+        Console.WriteLine("After transfer: ");
+        Console.WriteLine($"счет {account1.Id}: {account1.Balance}");
+        Console.WriteLine($"счет {account2.Id}: {account2.Balance}");
+
+        Console.ReadKey();
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Hillel C# homework 19
